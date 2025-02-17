a693999ee5ae615bef195602ffac7c30ebac1fc9
Maksudul <maaksuduldish@gmail.com>
2025-02-17 12:41:56 +0600

    Success modal show on going

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ const buttons = document.querySelectorAll('.seat-button');
 
 buttons.forEach(button => {
     button.addEventListener('click', (event) => {
-        button.style.backgroundColor = 'green';
+
         // Decrease total seat left
         const totalSeat = getElementNameById('seat-amount');
         const totalSeatText = totalSeat.innerText;
@@ -18,12 +18,43 @@ buttons.forEach(button => {
         // totalSelectedSeat.innerText = newSelectedSeat;
         const selectedSeat = getElementTextValueById('selected-seat');
         const newSelected = selectedSeat + 1;
-        setTextByValue('selected-seat', newSelected);
 
         // Showing total price
         const perTicketPrice = 550;
         totalTicketPrice = newSelected * perTicketPrice;
         setTextByValue('total-price', totalTicketPrice);
+        setTextByValue('grand-total', totalTicketPrice);
+
+        // Apply coupon code
+        const couponButton = getElementNameById('coupon-button');
+        couponButton.addEventListener('click', function () {
+            const couponInputField = getElementNameById('coupon-input');
+            const couponInputValue = couponInputField.value;
+
+            if (couponInputValue === 'NEW15') {
+                const discountedPrice = totalTicketPrice - (totalTicketPrice * (15 / 100));
+                setTextByValue('grand-total', discountedPrice);
+            }
+            else if (newSelected > 2 && couponInputValue === 'Couple20') {
+                const coupleDiscountedPrice = totalTicketPrice - (totalTicketPrice * (20 / 100));
+                setTextByValue('grand-total', coupleDiscountedPrice);
+            }
+            else {
+                setTextByValue('grand-total', totalTicketPrice);
+            }
+        })
+
+        // Maximum 4 tickets an user can select
+        if (newSelected > 4) {
+            button.removeEventListener('click');
+            button.style.backgroundColor = 'none'
+        }
+        else {
+            button.style.backgroundColor = 'green';
+        }
+
+        setTextByValue('selected-seat', newSelected);
+
 
         // Showing seat details
         const seatNameContainer = getElementNameById('seat-name');
@@ -40,5 +71,24 @@ buttons.forEach(button => {
         const seatPrice = document.createElement('p')
         seatPrice.innerText = perTicketPrice;
         seatPriceContainer.appendChild(seatPrice);
+
+        // Showing success modal
+        const passengerName = getElementNameById('passenger-input')
+        const passengerInputValue = passengerName.value.length;
+
+        const passengerPhone = getElementNameById('phone-input');
+        const phoneInputValue = passengerPhone.value.length;
+
+        const nextButton = getElementNameById('next-button');
+        const modalContainer = getElementNameById('modal-container');
+        const mainContainer = getElementNameById('main-container');
+
+        if (newSelected > 0 || passengerInputValue > 0 || phoneInputValue > 0) {
+            nextButton.addEventListener('click', function () {
+                mainContainer.classList.add = 'hidden'
+                modalContainer.classList.remove = 'hidden';
+            })
+        }
+
     })
 })
